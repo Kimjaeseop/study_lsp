f0e4b236fd3b443be882df81ea70089d9acbd61a
jaeseb96@naver.com <jaeseb96@naver.com>
2021-01-20 13:55:31 +0900

    2021.01.20 V0.4

---
+++ ch_4/ssu_tempnam.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+{
+	char *arg_directory = NULL;
+	char *arg_prefix = NULL;
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s <directory> <prefix>\n", argv[0]);
+		exit(1);
+	}
+
+	// 공백일 경우 처리
+	arg_directory = argv[1][0] != ' ' ? argv[1] : NULL;
+	arg_prefix = argv[2][0] != ' ' ? argv[2] : NULL;
+	printf("created : %s\n", tempnam(arg_directory, arg_prefix));
+
+	exit(0);
+}
