e523bf8cb6f33fcab1d2078eb2d34628bfdf9b05
jaeseb96@naver.com <jaeseb96@naver.com>
2021-01-27 19:16:18 +0900

    2021.01.27 V0.1

---
+++ ch_5/std.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+int main(void) 
+{
+int main(int argc, char *argv[]) 
+{
+	char *fname = "ssu_test.txt";
+
+	exit(0);
+}
